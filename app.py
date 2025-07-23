819ed055cce1f4d998bd95d024cb65c8797db530
Bhushan Sawant <bhushannsawant3303@gmail.com>
2025-07-24 01:25:47 +0530

    Local changes before pulling from GitHub

--- app.py
+++ app.py
@@ -68,4 +68,5 @@ def serve_chart(filename):
     return send_from_directory(app.config['STATIC_FOLDER_CHARTS'], filename)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
+
